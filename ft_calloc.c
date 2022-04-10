b9c5ed9500147c7898cfe55d385834f3e7101f97
Vladimir <94677889+Vladimir-Khlghatyan@users.noreply.github.com>
2022-04-10 15:22:56 +0400

    considering SIZE_MAX

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,16 +6,19 @@
 /*   By: vkhlghat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 19:24:20 by vkhlghat          #+#    #+#             */
-/*   Updated: 2022/03/17 17:43:14 by vkhlghat         ###   ########.fr       */
+/*   Updated: 2022/04/10 15:20:41 by vkhlghat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*dst;
 
+	if (size != 0 && SIZE_MAX / size < count)
+		return (NULL);
 	dst = malloc(count * size);
 	if (!dst)
 		return (NULL);
