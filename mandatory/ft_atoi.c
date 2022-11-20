aeaf479bded2e76016b65663922d86ab694fc15a
Vladimir <94677889+Vladimir-Khlghatyan@users.noreply.github.com>
2022-11-20 13:44:07 +0400

    update 20.11.2022

--- mandatory/ft_atoi.c
+++ mandatory/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vkhlghat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 20:40:10 by vkhlghat          #+#    #+#             */
-/*   Updated: 2022/03/12 22:21:37 by vkhlghat         ###   ########.fr       */
+/*   Updated: 2022/11/20 13:42:37 by vkhlghat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,12 @@ int	ft_atoi(const char *str)
 	num = 0;
 	sign = 1;
 	i = 0;
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || \
-				str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
 			i++;
 	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
+		if (str[i++] == '-')
 			sign = -1;
-		i++;
-	}
 	while (str[i] >= '0' && str[i] <= '9')
-	{
-		num = num * 10 + str[i] - '0';
-		i++;
-	}
+		num = num * 10 + str[i++] - '0';
 	return (num * sign);
 }
